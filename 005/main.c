aa649ede927293b75b1ffd38463286c0bbab1f47
Alex A. Mihaylov <alex_a_mihaylov@yahoo.com>
2015-10-15 14:34:10 +0300

    Add 005 task (shell)

---
+++ 005/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+
+#define BUFFER_SIZE	16384
+
+char inbuff[BUFFER_SIZE];
+char *path;
+
+void interrupt_callback(int signum)
+{
+    printf("\nshell finished.\n");
+    _exit(0);
+}
+
+int main(int argc, char** argv, char** env)
+{
+    signal(SIGINT, interrupt_callback);
+
+    /* Add pwd to PATH list */
+    path = getenv("PATH");
+    printf("old PATH=%s\n",path);
+    if(path) {
+	strcpy(inbuff, path);
+	strcat(inbuff,";.");
+    } else {
+	strcpy(inbuff,".");
+    }
+
+    if(setenv("PATH",inbuff,1)) {
+	perror("setenv");
+	return -1;
+    }
+    printf("new PATH=%s\n",inbuff);
+    
+    printf("simple shell started...\n");
+    while(1) {
+	int readed;
+	int cpid;
+	
+	write(STDOUT_FILENO, "shell> ",7);
+	
+	readed = read(STDIN_FILENO, inbuff, BUFFER_SIZE);
+	if(readed < 0) {
+	    perror("read");
+	    return -1;
+	};
+
+	/* exec write here */
+
+	write(STDOUT_FILENO, inbuff, readed);
+    }
+    return 0;
+}
